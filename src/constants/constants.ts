bcc2ea9a90de9fdb57d326a53c364b198bede1f1
Muhammad Naim Bin Roslan <heyitsnaim@gmail.com>
2025-08-10 21:35:40 +0800

    Removed supabase

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,10 +2,6 @@ export const API = {
   MAKANMANA: {
     HOSTNAME: process.env.MAKANMANA_BACKEND_URL,
   },
-  SUPABASE: {
-    HOSTNAME: process.env.SUPABASE_URL,
-    ANON: process.env.SUPABASE_ANON_KEY,
-  },
 };
 
 export const APP_VERSION = process.env.REACT_MAKANMANA_VERSION;
